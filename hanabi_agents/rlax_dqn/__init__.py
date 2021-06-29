3b7eea95adb97dff3edf9e2de606f37b4a2b1ad7
msagemue <m.sagemueller@campus.tu-berlin.de>
2021-06-29 20:08:26 +0200

    init added

--- hanabi_agents/rlax_dqn/__init__.py
+++ hanabi_agents/rlax_dqn/__init__.py
@@ -1,4 +1,4 @@
 #  from .rlax_dqn import DQNAgent
 from .rlax_rainbow import DQNAgent
-from .params import RlaxRainbowParams, RewardShapingParams, AgentType
+from .params import RlaxRainbowParams, RewardShapingParams, AgentType, PBTParams
 from .reward_shaping import RewardShaper, ShapingType
